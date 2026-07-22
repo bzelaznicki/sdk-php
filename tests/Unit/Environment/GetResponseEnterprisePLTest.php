cad1a3126d313b2a1774e483f7218dd3da86e67d
bzelaznicki <bartosz@zelaznicki.com>
2026-07-23 00:04:19 +0200

    test fixes

--- tests/Unit/Environment/GetResponseEnterprisePLTest.php
+++ tests/Unit/Environment/GetResponseEnterprisePLTest.php
@@ -13,58 +13,58 @@ use PHPUnit\Framework\TestCase;
  */
 class GetResponseEnterprisePLTest extends TestCase
 {
-    /**
-     * @var GetResponseEnterprisePL
-     */
-    private $systemUnderTest;
+	/**
+	 * @var GetResponseEnterprisePL
+	 */
+	private $systemUnderTest;
 
-    protected function setUp(): void
-    {
-        $this->systemUnderTest = new GetResponseEnterprisePL('custom-domain.getresponse360.pl');
-    }
+	protected function setUp(): void
+	{
+		$this->systemUnderTest = new GetResponseEnterprisePL('custom-domain.getresponse360.pl');
+	}
 
-    /**
-     * @test
-     */
-    public function shouldReturnUrl()
-    {
-        self::assertEquals('https://api3.getresponse360.pl', $this->systemUnderTest->getUrl());
-    }
+	/**
+	 * @test
+	 */
+	public function shouldReturnUrl()
+	{
+		self::assertEquals('https://api3.getresponse360.pl', $this->systemUnderTest->getUrl());
+	}
 
-    /**
-     * @test
-     */
-    public function shouldAddXDomainHeaderToRequestDuringProcessing()
-    {
-        $request = new Request('GET', 'https://api3.getresponse360.pl/v3/accounts');
+	/**
+	 * @test
+	 */
+	public function shouldAddXDomainHeaderToRequestDuringProcessing()
+	{
+		$request = new Request('GET', 'https://api3.getresponse360.pl/v3/accounts');
 
-        $processedRequest = $this->systemUnderTest->processRequest($request);
+		$processedRequest = $this->systemUnderTest->processRequest($request);
 
-        self::assertTrue($processedRequest->hasHeader('X-Domain'));
-        self::assertEquals('custom-domain.getresponse360.pl', $processedRequest->getHeaderLine('X-Domain'));
-    }
+		self::assertTrue($processedRequest->hasHeader('X-Domain'));
+		self::assertEquals('custom-domain.getresponse360.pl', $processedRequest->getHeaderLine('X-Domain'));
+	}
 
-    /**
-     * @test
-     * @dataProvider invalidDomainProvider
-     *
-     * @param string $domain
-     */
-    public function shouldThrowExceptionIfDomainIsNotValid($domain)
-    {
-        $this->expectException(InvalidDomainException::class);
-        new GetResponseEnterprisePL($domain);
-    }
+	/**
+	 * @test
+	 * @dataProvider invalidDomainProvider
+	 *
+	 * @param string $domain
+	 */
+	public function shouldThrowExceptionIfDomainIsNotValid($domain)
+	{
+		$this->expectException(InvalidDomainException::class);
+		new GetResponseEnterprisePL($domain);
+	}
 
-    /**
-     * @return array
-     */
-    public function invalidDomainProvider()
-    {
-        return [
-            ['invalid-domain'],
-            ['https://example.com'],
-            ['in:valid']
-        ];
-    }
+	/**
+	 * @return array
+	 */
+	public static function invalidDomainProvider()
+	{
+		return [
+			['invalid-domain'],
+			['https://example.com'],
+			['in:valid']
+		];
+	}
 }
--- tests/Unit/Environment/GetResponseEnterpriseUSTest.php
+++ tests/Unit/Environment/GetResponseEnterpriseUSTest.php
@@ -13,58 +13,58 @@ use PHPUnit\Framework\TestCase;
  */
 class GetResponseEnterpriseUSTest extends TestCase
 {
-    /**
-     * @var GetResponseEnterpriseUS
-     */
-    private $systemUnderTest;
+	/**
+	 * @var GetResponseEnterpriseUS
+	 */
+	private $systemUnderTest;
 
-    protected function setUp(): void
-    {
-        $this->systemUnderTest = new GetResponseEnterpriseUS('custom-domain.getresponse360.com');
-    }
+	protected function setUp(): void
+	{
+		$this->systemUnderTest = new GetResponseEnterpriseUS('custom-domain.getresponse360.com');
+	}
 
-    /**
-     * @test
-     */
-    public function shouldReturnUrl()
-    {
-        self::assertEquals('https://api3.getresponse360.com', $this->systemUnderTest->getUrl());
-    }
+	/**
+	 * @test
+	 */
+	public function shouldReturnUrl()
+	{
+		self::assertEquals('https://api3.getresponse360.com', $this->systemUnderTest->getUrl());
+	}
 
-    /**
-     * @test
-     */
-    public function shouldAddXDomainHeaderToRequestDuringProcessing()
-    {
-        $request = new Request('GET', 'https://api3.getresponse360.com/v3/accounts');
+	/**
+	 * @test
+	 */
+	public function shouldAddXDomainHeaderToRequestDuringProcessing()
+	{
+		$request = new Request('GET', 'https://api3.getresponse360.com/v3/accounts');
 
-        $processedRequest = $this->systemUnderTest->processRequest($request);
+		$processedRequest = $this->systemUnderTest->processRequest($request);
 
-        self::assertTrue($processedRequest->hasHeader('X-Domain'));
-        self::assertEquals('custom-domain.getresponse360.com', $processedRequest->getHeaderLine('X-Domain'));
-    }
+		self::assertTrue($processedRequest->hasHeader('X-Domain'));
+		self::assertEquals('custom-domain.getresponse360.com', $processedRequest->getHeaderLine('X-Domain'));
+	}
 
-    /**
-     * @test
-     * @dataProvider invalidDomainProvider
-     *
-     * @param string $domain
-     */
-    public function shouldThrowExceptionIfDomainIsNotValid($domain)
-    {
-        $this->expectException(InvalidDomainException::class);
-        new GetResponseEnterpriseUS($domain);
-    }
+	/**
+	 * @test
+	 * @dataProvider invalidDomainProvider
+	 *
+	 * @param string $domain
+	 */
+	public function shouldThrowExceptionIfDomainIsNotValid($domain)
+	{
+		$this->expectException(InvalidDomainException::class);
+		new GetResponseEnterpriseUS($domain);
+	}
 
-    /**
-     * @return array
-     */
-    public function invalidDomainProvider()
-    {
-        return [
-            ['invalid-domain'],
-            ['https://example.com'],
-            ['in:valid']
-        ];
-    }
+	/**
+	 * @return array
+	 */
+	public static function invalidDomainProvider()
+	{
+		return [
+			['invalid-domain'],
+			['https://example.com'],
+			['in:valid']
+		];
+	}
 }
